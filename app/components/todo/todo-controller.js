b607444dc379f2130ac13aef66a330c3e25d78ae
Seth Rossiter <strossiter87@gmail.com>
2017-05-18 13:21:36 -0600

    changes

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,100 +1,56 @@
-function TodoController() {
+
+function TodoController(){
 	// new up the TodoService that has already been configured for your use
 	// There are two methods getTodos returns and array
-	 // saveTodos accepts an array and stores it to your local storage
+	// saveTodos accepts an array and stores it to your local storage
 	var todoService = new TodoService()
 
-	this.addTodoFromForm = function addTodoFromForm (e) {
+	this.addTodoFromForm = function(e) {
 		e.preventDefault()
-		// TAKE THE INFORMATION FORM THE FORM
+		// TAKE THE INFORMATION FROM THE FORM
 		var form = e.target
-
 		//ADD IT TO THE ARRAY BELOW
 		todosArr = todoService.getTodos()
 		todosArr.push(form.todo.value)
-		localstorage[todo]= JSON.stringify(mylist)
-
 		// FINAL ACTION OF ADDING A TODO
-		todoService.saveTodos(todosArr)
+		todoService.saveTodos(todosArr)	
+		// 
 		drawTodos(todosArr)
 		document.getElementById("todo-form").reset()
-	}
+    }
 
-	this.removeTodoFromForm = function removeTodoFromForm (todo) {
+	this.removeTodoFromForm = function removeTodoFromForm(todo){
+
 		todosArr = todoService.getTodos()
-		todosArr.splice(todoArr.indexOf(todo),1)
+		// for(var i = 0; i < todosArr.length; i++){
+		// 	todo = todoArr[i]
+		// }
+			
+		todosArr.splice(todosArr.indexOf(todo),1)
 		todoService.saveTodos(todosArr)
 		drawTodos(todoService.getTodos())
 	}
-	function drawTodos(todos) {
+
+	
+    function drawTodos(todos){
+
 		var element = document.getElementById('todo')
 		var template = ''
-		for (var i = 0; i < todos.length; i++) {
-			var todo = todo[i]
-			template += `
+		for(var i = 0; i < todos.length; i++){
+
+			todo = todos[i]
+			template +=
+			`
 			<div class="content-todo">
-				<h3>${todo}</h3>
+				<h3>${todo}
 				<button onclick="app.controllers.toDoController.removeTodoFromForm(event)">X</button>
-			</div>
+			</div>		
+						
 			`
 		}
 		element.innerHTML = template
 	}
-	drawTodos(todoService.getTodos())
+    drawTodos(todoService.getTodos())
 }
-
-// function get_todos() {
-//     var todos = new Array;
-//     var todos_str = localStorage.getItem('todo');
-//     if (todos_str !== null) {
-//         todos = JSON.parse(todos_str); 
-//     }
-//     return todos;
-// }
- 
-// function add() {
-//     var task = document.getElementById('task').value;
- 
-//     var todos = get_todos();
-//     todos.push(task);
-//     localStorage.setItem('todo', JSON.stringify(todos));
- 
-//     show();
- 
-//     return false;
-// }
- 
-// function remove() {
-//     var id = this.getAttribute('id');
-//     var todos = get_todos();
-//     todos.splice(id, 1);
-//     localStorage.setItem('todo', JSON.stringify(todos));
- 
-//     show();
- 
-//     return false;
-// }
- 
-// function show() {
-//     var todos = get_todos();
- 
-//     var html = '<ul>';
-//     for(var i=0; i<todos.length; i++) {
-//         html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
-//     };
-//     html += '</ul>';
- 
-//     document.getElementById('todos').innerHTML = html;
- 
-//     var buttons = document.getElementsByClassName('remove');
-//     for (var i=0; i < buttons.length; i++) {
-//         buttons[i].addEventListener('click', remove);
-//     };
-// }
- 
-// document.getElementById('add').addEventListener('click', add);
-// show();
-
-
 
 
